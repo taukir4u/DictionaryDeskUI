d848ea998d92fc616629cdb65bf9111ed5de44a7
taukir4u <taukir4u@gmail.com>
2014-12-26 19:56:23 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ DictionaryDeskUI/DictionaryAppUI.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DictionaryDeskUI
+{
+    partial class DictionaryAppUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.detailsTextBox = new System.Windows.Forms.TextBox();
+            this.searchIdTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(63, 29);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(207, 20);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // detailsTextBox
+            // 
+            this.detailsTextBox.Location = new System.Drawing.Point(63, 81);
+            this.detailsTextBox.Multiline = true;
+            this.detailsTextBox.Name = "detailsTextBox";
+            this.detailsTextBox.Size = new System.Drawing.Size(207, 42);
+            this.detailsTextBox.TabIndex = 1;
+            // 
+            // searchIdTextBox
+            // 
+            this.searchIdTextBox.Location = new System.Drawing.Point(63, 186);
+            this.searchIdTextBox.Name = "searchIdTextBox";
+            this.searchIdTextBox.Size = new System.Drawing.Size(207, 20);
+            this.searchIdTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(276, 100);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(276, 184);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 7;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(137, 245);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 8;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // DictionaryAppUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 294);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchIdTextBox);
+            this.Controls.Add(this.detailsTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Name = "DictionaryAppUI";
+            this.Text = "DictionaryAppUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox detailsTextBox;
+        private System.Windows.Forms.TextBox searchIdTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button showAllButton;
+    }
+}
+
---
+++ DictionaryDeskUI/DictionaryAppUI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionaryDeskUI
+{
+    public partial class DictionaryAppUI : Form
+    {
+
+        Dictionary<string, string> studentIfo = new Dictionary<string, string>();
+        public DictionaryAppUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            studentIfo.Add(idTextBox.Text, detailsTextBox.Text);
+            MessageBox.Show("Information saved");
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            bool id = studentIfo.ContainsKey(searchIdTextBox.Text);
+            if (id)
+            {
+                string details = studentIfo[searchIdTextBox.Text];
+                MessageBox.Show(details,"Search Result: ");
+            }
+            else
+            {
+                MessageBox.Show("Not found");
+            }
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            string info = "";
+            foreach (KeyValuePair<string, string> item in studentIfo)
+            {
+                info += (item.Key + " " + item.Value +" \n");
+            }
+            MessageBox.Show(info);
+            }
+    }
+}
